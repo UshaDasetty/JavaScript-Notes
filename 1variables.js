eb26f56f4c2aef0ca86434639a8cea794bf3c7a1
UshaDasetty <ushadasetty@gmail.com>
2022-06-01 13:23:22 +0530

    NodeJS

---
+++ 1variables.js
@@ -0,0 +1,24 @@
+var fullName = "Usha Dasetty";
+console.log(fullName);  
+console.log(typeof(fullName));  // string
+
+var Course = "Javascript";
+console.log(Course);
+console.log(typeof(Course));    // string
+
+var isLoggedIn = false;
+console.log(isLoggedIn);
+console.log(typeof(isLoggedIn));   // boolean
+
+var loggedCount = 10;
+console.log(loggedCount);
+console.log(typeof(loggedCount));   // number
+
+var paymentMethod;
+console.log(paymentMethod);            // undefined
+console.log(typeof(paymentMethod));   //undefined
+paymentMethod = "DebitCard";
+console.log(paymentMethod);     // DebitCard
+
+var lastName = {};     // null
+console.log(typeof(lastName));  // object
---
+++ 2Interpollation.js
@@ -0,0 +1,16 @@
+// Interpollation: concating string with variable using ` ${}`
+
+const uid = 'abc123';
+var fullName = 'Usha Dasetty';
+var email = 'usha@gmail.com';
+var password = '123456';
+var confirmPassword = '123456';
+var isLoggedIn = false;
+
+// Interpollation
+console.log(`
+    uid: ${uid}
+    fullname: ${fullName}
+    email: ${email}
+    password: ${password}
+`)
